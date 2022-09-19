fa7dbe81d270dd27dee100fdb6db89cdc5b518b4
Macq Quentin <macqquentin@hotmail.fr>
2022-09-19 16:54:48 +0200

    squelletes dashbaord

---
+++ views/headerdashboard.php
@@ -0,0 +1,14 @@
+<div class="dashboardHeader">
+    <p>
+        <title>
+            <h3>Dashboard</h3>
+        </title>
+    </p>
+    <p>dashboard . include"page"</p>
+</div>
+<div class="Welcome">
+    <p>
+        Welcome back . Include "login administrateur"
+    </p>
+    <p class="imagewelcome"><img src="" alt=""></p>
+</div>
