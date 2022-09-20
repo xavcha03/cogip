3409d46a10effb5df34509ea5dbb63b4f1e7ffc0
xavcha03 <ch.xavier03@gmail.com>
2022-09-20 12:35:28 +0200

    first routes and views

--- index.php
+++ index.php
@@ -5,15 +5,3 @@ use Bramus\Router\Router;
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/Core/Helper.php';
 require_once __DIR__ . '/Routes/Routes.php';
-
-//Create Router instance
-$router = new Router;
-
-//Define routes
-
-
-$router->post('/dashboard', function () {
-    echo "Add a new invoice";
-});
-//Run router
-$router->run();
